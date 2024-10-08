bb849fe209020f9295bfd3c4d413ae5c983f3256
Пользователь <abdullaevabubakir@icloud.com>
2024-10-08 17:33:20 +0300

    first

---
+++ car.dart
@@ -0,0 +1,21 @@
+
+class Car {
+  String model;
+  int year;
+  List<Engine> engine;
+  Car({
+    required this.model,
+    required this.year,
+    required this.engine,
+  });
+}
+
+class Engine {
+  int abyom;
+  int liyer;
+  Engine({
+    required this.abyom,
+    required this.liyer,
+  });
+
+}
---
+++ lesson8.dart
@@ -0,0 +1,61 @@
+
+
+
+import 'car.dart';
+
+void main() {
+  // tip danih
+  // String
+  // int
+  // List<>
+  // var
+  // double
+  // bool
+  // dynamic
+
+  int age = 13;
+  if (age < 15) {
+  } else if (age > 50 && age > 12){
+  } else if (age == 21 || age > 12){
+  } else {}
+
+
+  switch (age) {
+    case 12:
+      break;
+    case 20:
+      break;
+  }
+
+
+  Engine engine = Engine(abyom: 123, liyer: 123);
+
+  Car car = Car(model: "model", year: 2012, engine: [engine, engine]);
+
+  Loop();
+}
+
+int sum(a, b) {
+  if (a < 10){
+    return 10;
+  }else if (b < 10) {
+  return 20;
+  } else {
+    return 12;
+  }
+
+}
+
+void Loop() {
+  for (var i = 0; i < 3; i++){
+    for (var element in [1, 2, 3]){
+      print(element);
+    }
+  }
+
+  int a = 10;
+  while (a < 20){
+    print(a);
+    a++;
+  }
+}
